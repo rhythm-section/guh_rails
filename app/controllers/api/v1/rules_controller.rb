c825f3b03db92cb19ffcd8401564e0b615c57e17
wukerplank <wukerplank@edthofer.at>
2014-12-02 18:05:23 +0100

    Uses the latest guh.rb gem and adds endpoints to view and delete rules.

--- app/controllers/api/v1/rules_controller.rb
+++ app/controllers/api/v1/rules_controller.rb
@@ -8,11 +8,27 @@ class Api::V1::RulesController < ApplicationController
     end
   end
 
+  def show
+    @rule = Guh::Rule.find(params[:id])
+
+    respond_to do |format|
+      format.json { render json: @rule }
+    end
+  end
+
   def create
     @rule = Guh::Rule.add(params[:rule])
 
     respond_to do |format|
       format.json { render json: @rule }
+    end
+  end
+
+  def destroy
+    @response = Guh::Rule.remove(params[:id])
+
+    respond_to do |format|
+      format.json { render json: @response }
     end
   end
 
